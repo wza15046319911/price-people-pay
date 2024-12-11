9e3ba44c75f42db8962309f92246d9f016a46d57
zianwang <wza15046319911@github.com>
2024-12-11 16:46:16 +1100

    feature: default to login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import Login from './pages/Login'
 import Homepage from './pages/Homepage'
 import axios from './lib/requests'
@@ -14,8 +14,10 @@ function App() {
     >
       <BrowserRouter>
         <Routes>
+          <Route path="/" element={<Navigate to="/login" replace />} />
           <Route path="/login" element={<Login />} />
-          <Route path="/" element={<Homepage />} />
+          <Route path="/home" element={<Homepage />} />
+          <Route path="*" element={<Navigate to="/login" replace />} />
         </Routes>
       </BrowserRouter>
     </SWRConfig>
