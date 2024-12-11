32fdc037e34656c529b62013054a01a0b0bf3d46
zianwang <wza15046319911@github.com>
2024-12-11 16:31:39 +1100

    feature: develop Login page and Search result page

--- src/lib/requests.ts
+++ src/lib/requests.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:3000',
+    baseURL: process.env.API_BASE_URL,
     timeout: 10000,
     headers: {
         'Content-Type': 'application/json',
