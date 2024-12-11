ba4abe00829db57091c8369df3322a764195e35a
zianwang <wza15046319911@github.com>
2024-12-11 17:12:39 +1100

    fix: login and homepage request url

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -11,7 +11,7 @@ function Login() {
   const handleLogin = async () => {
     try {
       setError('')
-      const response = await axios.post(`${process.env.VITE_API_BASE_URL}/login`, {
+      const response = await axios.post(`${import.meta.env.VITE_API_BASE_URL}/login`, {
         username,
         password
       })
